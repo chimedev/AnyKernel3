37971a8341c9365d6312546467348b43ea20bbfb
HeroBuxx <me@buxxed.tk>
2022-12-10 21:40:31 +0700

    Changes for chime
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Lizard Kernel 4.19 for chime
+kernel.string=lunatickernel4chime
 do.devicecheck=1
 do.modules=0
 do.systemless=1
